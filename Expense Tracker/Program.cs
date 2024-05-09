5c11445b67157c1c7fb8a0562e8ba36519c3851c
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-05-09 16:09:09 -0400

    First migration done.

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //DI
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("connection")));
 
 var app = builder.Build();
 
